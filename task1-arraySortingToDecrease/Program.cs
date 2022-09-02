8cfb512328f2d27a4023479c88197abc4f566404
Ryan <llryanll@yandex.ru>
2022-09-02 23:31:38 +0300

    task1 - solved

--- task1-arraySortingToDecrease/Program.cs
+++ task1-arraySortingToDecrease/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 
+*/
+
+int[,] FillArray (int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void SortingArray(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i,k] = temp;
+                }
+            }
+            Console.Write(array[i,j] + " ");
+        }
+         Console.WriteLine();
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+           for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = FillArray(4, 4, 0, 10);
+Console.WriteLine("Исходный массив: ");
+PrintArray(array);
+Console.WriteLine("Отсортированный массив: ");
+SortingArray(array);
